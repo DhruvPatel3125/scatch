836bbedba954a015facab2b694daefcc95094e76
dhruv <dhruvjpatel5@gmail.com>
2025-01-12 17:45:39 +0530

    `feat: updated authController, routes, models, and views for logout functionality and product creation`

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const isLoggedin = require("../middlewares/isLoggedin");
-
-// Root route
-router.get("/", function (req, res) {
+const isLoggedin = require("../middlewares/isLoggedin")
+router.get("/",function(req,res){
     let error = req.flash("error");
-    res.render("index", { error });
-});
+    res.render("index",{error});
+})
 
-// Shop route with middleware
-router.get("/shop", isLoggedin, function (req, res) {
-    res.render("shop");
-});
+router.get("/shop",isLoggedin,function(req,res){
+    res.render("shop")
+}
+)
+router.get("/logout",isLoggedin,function(req,res){
+    res.render("shop")
+}
+)
 
 module.exports = router;
